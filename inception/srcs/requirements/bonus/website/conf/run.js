bcb1dd13f08b89d3af5b86df2521ad09ee29e7f9
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2024-02-02 15:39:51 +0100

    Add files via upload

--- inception/srcs/requirements/bonus/website/conf/run.js
+++ inception/srcs/requirements/bonus/website/conf/run.js
@@ -1,9 +1,46 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('index.html').pipe(res)
-})
+    // Construit le chemin du fichier demandé.
+    let filePath = '.' + req.url;
+    if (filePath == './') {
+        filePath = './index.html';
+    }
 
-server.listen(process.env.PORT || 3000)
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+    };
+
+    const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            }
+            else {
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(process.env.PORT || 3000);
